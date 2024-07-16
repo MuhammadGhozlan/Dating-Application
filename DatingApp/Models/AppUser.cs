0f81af26aebf243a0aa59eb1c83b614f8dcc73ae
Muhammad.Ghozlan@yahoo.com <Muhammad.Ghozlan@yahoo.com>
2024-07-15 22:01:05 -0500

    initial commit

---
+++ DatingApp/Models/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DatingApp.Models
+{
+    public class AppUser
+    {
+        [Key]
+        public int Id { get; set; }
+        [AllowNull, MaxLength(50), ]
+        public string Username { get; set; }
+    }
+}
---
+++ DatingApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DatingApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
